2bef0f6a2789ccf775df211f8398bf7efd65a20c
DanielKnourek <dev.danielknourek@gmail.com>
2022-03-15 14:03:23 +0000

    removing test test testing test

--- frontend/lib/complexMath.js
+++
@@ -1,8 +0,0 @@
-
-// export const funSum = (a, b) => {
-const funSum = (a, b) => {
-    digits_b = Math.ceil(Math.log10(Math.abs(b)))
-    return (a * Math.pow(10, digits_b + 1) + b)
-}
-
-export default funSum;
