e097a482ff57b73569ec0cdb26cc637e7acfd01a
DanielKnourek <dev.danielknourek@gmail.com>
2022-03-15 14:17:33 +0000

    refactoring getHello test

--- tests/frontend/lib/getHello.test.ts
+++ tests/frontend/lib/getHello.test.ts
@@ -1,16 +1,9 @@
 import getHello, { Data } from 'frontend/lib/getHello'
-// import getHello, { Data } from '../../../frontend/lib/getHello'
 
 describe('testing api/test/hello', () => {
 
     test('is John Doe', () => {
-        expect(getHello()).toStrictEqual<Data>({name: 'John Doe'});
-        // const fake = getHello();
-        // expect("hello").toBe("hello");
+        expect(getHello().name).toBe<Data['name']>('John Doe');
     });
 
 });
-
-// required with this small example
-// to make the isolatedModules config happy
-// export { }
