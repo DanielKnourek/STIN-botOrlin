82286b6f95697e0ea90c87dc8bbc6c59dc0a72c2
DanielKnourek <dev.danielknourek@gmail.com>
2022-05-03 00:48:14 +0200

    preparation for sonarcloud

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -29,6 +29,7 @@ const config: Config.InitialOptions = {
     '<rootDir>/tests/',
   ],
   coverageDirectory: "tests/coverage",
+  coverageReporters: ["lcov", ["text", { "skipFull": true }]],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
     '/^.+.(css|less|scss|sass)$/': 'jest-css-modules-transform',
@@ -37,97 +38,12 @@ const config: Config.InitialOptions = {
     'node_modules/',
     '.next/',
   ],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config)
-
-
-
-
-// import type { Config } from '@jest/types';
-// import nextJest from 'next/jest';
-
-// const config: Config.InitialOptions = {
-//   preset: "ts-jest",
-//   globals: {
-//     'ts-jest': {
-//       isolatedModules: true,
-//     }
-//   },
-//   rootDir: "../../",
-//   // roots: [
-//   //   "frontend",
-//   //   "node_modules/frontend/lib",
-//   //   "../frontend/lib",
-//   // ],
-//   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-//   setupFilesAfterEnv: ['<rootDir>/tests/config/setupTests.js'],
-//   moduleNameMapper: {
-//     '\\.(css|less|scss|sass)$': 'jest-css-modules-transform',
-//   },
-//   transform: {
-//     '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
-//     '/^.+.(css|less|scss|sass)$/': 'jest-css-modules-transform',
-//   },
-//   transformIgnorePatterns: [
-//     'node_modules/',
-//     '.next/',
-//   ],
-//   collectCoverage: true,
-//   collectCoverageFrom: [
-//     "**/*.{js,jsx,ts,tsx}",
-//   ],
-//   coveragePathIgnorePatterns: [
-//     'node_modules/',
-//     '.next/',
-//     '<rootDir>/tests/',
-//   ],
-  
-//   coverageReporters: ["lcov", ["text", { "skipFull": true }]],
-//   coverageDirectory: "tests/coverage",
-//   coverageThreshold: {
-//     global: {
-//       lines: 85,
-//     },
-//   },
-//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-//   testEnvironment: "jest-environment-jsdom",
-//   // testEnvironment: "node",
-
-//   // // setupFilesAfterEnv: ["jest-extended/all"],
-//   // // testPathIgnorePatterns: [
-//   // //   "<rootDir>/**/node_modules/"
-//   // // ],
-//   // // testPathIgnorePatterns: ["<rootDir>/tests/fixtures/"],
-//   // // testEnvironment: 'node',
-
-//   // testResultsProcessor: "jest-sonar-reporter",
-//   // // "coveragePathIgnorePatterns": [
-//   // //   "/node_modules/",
-//   // //   "/test/"
-//   // // ],
-
-//   // moduleNameMapper: {
-//   //   '^(.*)\\.js$': '$1',
-//   // },
-//   // testEnvironment: 'jest-environment-node',
-//   // transform: {
-//   //   "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
-//   // },
-//   // transformIgnorePatterns: [
-//   //   'node_modules/(?!aggregate-error|clean-stack|escape-string-regexp|indent-string|p-map)',
-//   //   '/.next',
-//   // ],
-//   // automock: true,
-//   verbose: true,
-// };
-
-// const createJestConfig = nextJest({
-//   dir: "./",
-// });
-
-// // const jestConfig = createJestConfig(config);
-// // export default jestConfig;
-// // module.exports = jestConfig;
-// export default config;
+module.exports = createJestConfig(config);
